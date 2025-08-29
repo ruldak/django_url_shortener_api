1671f7c872d99136299cd69989177da12c1fdaaa
ruldak <ruldak3306@gmail.com>
2025-08-29 19:37:52 +0700

    add api/ at the beginning of the url

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from app.views import RegisterView, Login, RefreshToken, LinkListCreateView, LinkDetailView, LinkRedirectView, LinkAnalyticsView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('api/register/', RegisterView.as_view(), name='register'),
     path('api/token/', Login.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', RefreshToken.as_view(), name='token_refresh'),
 
     path('api/links/', LinkListCreateView.as_view(), name='link-list-create'),
     path('api/links/<str:short_code>/', LinkDetailView.as_view(), name='link-detail'),
     path('api/links/<str:short_code>/analytics/', LinkAnalyticsView.as_view(), name='link-analytics'),
-    path('r/<str:short_code>/', LinkRedirectView.as_view(), name='link-redirect'),
+    path('api/r/<str:short_code>/', LinkRedirectView.as_view(), name='link-redirect'),
 ]
